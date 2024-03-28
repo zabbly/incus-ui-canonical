4223123fb06088fa2d16d8c7d9b8575fb8769b62
Stéphane Graber <stgraber@stgraber.org>
2024-03-28 15:54:43 +0000

    Improve OpenFGA support
    This makes a few changes to better work with OpenFGA:
     - Don't expect a clear 404 on a non-existing object
    
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    License: GPL-3.0-only (without Canonical CLA)

--- src/util/helpers.tsx
+++ src/util/helpers.tsx
@@ -253,7 +253,7 @@ const _checkDuplicateName = async (
       signal,
     },
   ).then((response) => {
-    if (response.status === 404) {
+    if (response.status === 404 || response.status === 403) {
       validNameCache[cacheKey].push(candidate);
       return true;
     } else {
