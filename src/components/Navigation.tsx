16ebb92667d457e791f563563ceaea22d8f7b454
Stéphane Graber <stgraber@stgraber.org>
2024-08-03 04:06:54 +0000

    Skip LXD identity API
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>
    License: GPL-3.0-only (without Canonical CLA)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -610,7 +610,7 @@ const Navigation: FC = () => {
                     <SideNavigationItem>
                       <div
                         className="p-side-navigation__link"
-                        title={`${loggedInUserName} (${loggedInUserID})`}
+                        title={`${loggedInUserName}`}
                       >
                         {authMethod == "tls" ? (
                           <Icon
