f7471d53b9658c04ef5ebc386715d84b8e7d9696
Piotr Resztak <piotr.resztak@futurfusion.io>
2024-12-02 16:34:00 +0000

    Fix displaying resources usage data for instances
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>

--- src/util/metricSelectors.tsx
+++ src/util/metricSelectors.tsx
@@ -25,8 +25,8 @@ export const getInstanceMetrics = (
       .find((item) => item.name === metricKey)
       ?.metrics.find((item) => item.labels.name === instance.name)?.value;
 
-  const memFree = memValue("lxd_memory_MemFree_bytes");
-  const memTotal = memValue("lxd_memory_MemTotal_bytes");
+  const memFree = memValue("incus_memory_MemFree_bytes");
+  const memTotal = memValue("incus_memory_MemTotal_bytes");
   const memory =
     memFree && memTotal
       ? {
@@ -43,8 +43,8 @@ export const getInstanceMetrics = (
           item.labels.name === instance.name && item.labels.mountpoint === "/",
       )?.value;
 
-  const diskFree = diskValue("lxd_filesystem_free_bytes");
-  const diskTotal = diskValue("lxd_filesystem_size_bytes");
+  const diskFree = diskValue("incus_filesystem_free_bytes");
+  const diskTotal = diskValue("incus_filesystem_size_bytes");
   const disk =
     diskFree && diskTotal
       ? {
