fc85ebb197496eb10f601a02dc3da35f8dd8a275
Piotr Resztak <piotr.resztak@futurfusion.io>
2025-04-02 23:09:25 +0200

    Move instance filtering logic from client to server
    Signed-off-by: Piotr Resztak <piotr.resztak@futurfusion.io>
    License: GPL-3.0-only (without Canonical CLA)

--- src/util/instanceFilter.tsx
+++ src/util/instanceFilter.tsx
@@ -65,16 +65,6 @@ const shorthandFunctions: Record<
   string,
   (instance: LxdInstance, value: string) => boolean
 > = {
-  type: (instance: LxdInstance, value: string) =>
-    instance.type.toLowerCase() == value.toLowerCase(),
-  state: (instance: LxdInstance, value: string) =>
-    instance.status.toLowerCase() == value.toLowerCase(),
-  status: (instance: LxdInstance, value: string) =>
-    instance.status.toLowerCase() == value.toLowerCase(),
-  architecture: (instance: LxdInstance, value: string) =>
-    instance.architecture.toLowerCase() == value.toLowerCase(),
-  location: (instance: LxdInstance, value: string) =>
-    instance.location.toLowerCase() == value.toLowerCase(),
   ipv4: (instance: LxdInstance, value: string) => matchByIpv4(instance, value),
   ipv6: (instance: LxdInstance, value: string) => matchByIpv6(instance, value),
 };
@@ -126,48 +116,6 @@ const evaluateShorthandFilter = (
   return false;
 };
 
-const dotPrefixMatch = (shortKey: string, fullKey: string): boolean => {
-  const fullMembs = fullKey.split(".");
-  const shortMembs = shortKey.split(".");
-
-  if (fullMembs.length != shortMembs.length) {
-    return false;
-  }
-
-  for (let i = 0; i < fullMembs.length; i++) {
-    if (!fullMembs[i].startsWith(shortMembs[i])) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const evaluateExpandedConfig = (
-  key: string,
-  value: string,
-  instance: LxdInstance,
-): boolean => {
-  for (const configKey in instance.expanded_config) {
-    if (dotPrefixMatch(key, configKey)) {
-      if (!(value.includes("^") || value.includes("$"))) {
-        value = `^${value}$`;
-      }
-
-      try {
-        const regex = new RegExp(value, "i");
-        if (regex.test(instance.expanded_config[configKey] || "")) {
-          return true;
-        }
-      } catch {
-        continue;
-      }
-    }
-  }
-
-  return false;
-};
-
 export const showInstance = (
   instance: LxdInstance,
   filters: string[],
@@ -175,40 +123,9 @@ export const showInstance = (
   for (let i = 0; i < filters.length; i++) {
     const filter = filters[i].split("=");
     const key = filter[0];
-
-    if (filter.length == 1) {
-      if (key === "") {
-        continue;
-      }
-
-      let regexpValue = key;
-      if (!(regexpValue.includes("^") || regexpValue.includes("$"))) {
-        regexpValue = `^${regexpValue}$`;
-      }
-
-      try {
-        const regex = new RegExp(regexpValue, "i");
-        if (regex.test(instance.name)) {
-          continue;
-        }
-      } catch {
-        // eslint-disable-next-line no-empty
-      }
-
-      if (instance.name.startsWith(key)) {
-        continue;
-      }
-
-      return false;
-    }
-
     const value = filter[1];
 
     if (evaluateShorthandFilter(key, value, instance) == true) {
-      continue;
-    }
-
-    if (evaluateExpandedConfig(key, value, instance) == true) {
       continue;
     }
 
@@ -234,9 +151,29 @@ export const prepareInstanceServerSideFilters = (
 ): string[] => {
   return filters.map((filter) => {
     const items = filter.split("=");
-    if (items.length == 2 && items[0] == "state") {
-      return `status=${items[1]}`;
+
+    if (items.length == 1) {
+      let regexpValue = items[0];
+
+      if (!(regexpValue.includes("^") || regexpValue.includes("$"))) {
+        regexpValue = `^${regexpValue}$`;
+      }
+
+      return `name=(${regexpValue}|^${items[0]}.*)`;
     } else {
+      let firstPart = items[0];
+      if (firstPart.includes(".")) {
+        firstPart = firstPart.split(".")[0];
+      }
+
+      if (!serverSupportedFields.includes(firstPart)) {
+        return `expanded_config.${filter}`;
+      }
+
+      if (items[0] == "state") {
+        return `status=${items[1]}`;
+      }
+
       return filter;
     }
   });
@@ -244,6 +181,7 @@ export const prepareInstanceServerSideFilters = (
 
 export const getServerSupportedFilters = (
   filters: string[],
+  clientSupportedFilters: string[],
 ): [string[], string[]] => {
   const serverFilters: string[] = [];
   const clientFilters: string[] = [];
@@ -251,12 +189,12 @@ export const getServerSupportedFilters = (
   filters.forEach((filter) => {
     const items = filter.split("=");
 
-    if (
-      items.length < 2 ||
-      items[0].includes(".") ||
-      items[1].includes(",") ||
-      !serverSupportedFields.includes(items[0])
-    ) {
+    if (items.length == 1) {
+      serverFilters.push(filter);
+      return;
+    }
+
+    if (clientSupportedFilters.includes(items[0])) {
       clientFilters.push(filter);
     } else {
       serverFilters.push(filter);
